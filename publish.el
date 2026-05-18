00979170c345c2dd12d0d7a7fd8e0534c4668622
Perry Smith <pedz@easesoftware.com>
2026-05-18 15:59:17 -0500

    Adding publish.el
    I think I need to add it to git even though it isn't finished so it is
    considered part of this project.

---
+++ publish.el
@@ -0,0 +1,26 @@
+;;; publish.el --- code to publish this project -*- lexical-binding:t -*-
+
+;; This is code so that I can execute one command and the project will
+;; get "published".  Currently that will be to export index.org, Canon
+;; EOS R1 Suppliment.org, and Canon EOS R5 Mark II Suppliment.org to
+;; HTML.  I have hopes of also publishing epub files.
+;;
+;; When the repo is pushed to GitHub, the GitHub Pages will trigger a
+;; build which results in the three html files being made available
+;; publicly on the net.
+
+(defun pedz/publish-canon-project ()
+  "Publish the Canon-EOS-Documentation-Project"
+  (interactive)
+  (setq org-babel-inline-result-wrap "%s"
+        org-confirm-babel-evaluate nil
+        org-publish-project-alist
+        '(("Canon-EOS-Documentation-Project"
+           :base-directory "~/Source/Canon-EOS-Documentation-Project"
+           :publishing-function org-html-publish-to-html
+           :publishing-directory "~/Source/Canon-EOS-Documentation-Project"
+           :exclude "*.org"
+           :include ("Canon EOS R1 Suppliment.org"
+                     "Canon EOS R5 Mark II Suppliment.org"
+                     "index.org"))))
+  (org-publish-current-project))
